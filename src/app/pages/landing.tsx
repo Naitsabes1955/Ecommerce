ef1bfc097563e73aaec4e3ebdc21675b661114d5
Naitsabes1955 <juansebasmosquera519@gmail.com>
2026-06-10 11:57:15 -0500

    initialize project

---
+++ src/app/pages/landing.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+export default function Home(){
+
+    return(
+        <>
+        <header className="relative">
+            <div className="mx-auto max-w-10xl px-2 sm:px-6 lg:px-8 bg-sky-600">
+                <div className="relative flex h-16 items-center justify-between">
+                    <div className="absolute inset-y-0 left-0 flex items-center">
+                        <span className="text-black font-semibold font-sans">LOGO</span>
+                        <button className="relative inline-flex items-center justify-center rounded-md p-2  hover:text-white "></button>
+                        <span></span>
+                </div>
+            </div>
+        </div>
+        </header>
+        </>
+    )
+}
